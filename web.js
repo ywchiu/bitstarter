0b316b607621e628aec4a58b79ed40530b951baf
ywchiu <tr.ywchiu@gmail.com>
2013-07-10 17:17:33 +0800

    add index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    response.writeHead(200, { 'Content-type': 'text/html'});
+    response.end(fs.readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
